df2c0931e5f56849953e991ed93ccac5daf77f95
Tim Fletcher <tim@night-shade.org.uk>
2019-05-19 14:56:23 +0200

    Upgrade to the latest version of Samba

--- samba-timemachine/spec/samba-timemachine/samba-timemachine_spec.rb
+++ samba-timemachine/spec/samba-timemachine/samba-timemachine_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 # Define packages
 packages = {
   'samba' => {
-    version: '2:4.9.5+dfsg-3'
+    version: '2:4.9.5+dfsg-4'
   },
   'samba-vfs-modules' => {
-    version: '2:4.9.5+dfsg-3'
+    version: '2:4.9.5+dfsg-4'
   }
 }
 
