aeeaa0dc9accb4168215e8025c97c7fc31ac361b
Tim Fletcher <tim.fletcher@gmail.com>
2019-11-19 22:48:07 +0100

    Enable multiarch support with a shell wrapper for supercronic

---
+++ offlineimap/supercronic-install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -eu -o pipefail
+
+VERSION=v0.1.9
+
+amd64(){
+  export SUPERCRONIC=supercronic-linux-amd64
+  export SUPERCRONIC_SHA1SUM=5ddf8ea26b56d4a7ff6faecdd8966610d5cb9d85
+}
+
+arm(){
+  export SUPERCRONIC=supercronic-linux-arm
+  export SUPERCRONIC_SHA1SUM=47481c3341bc3a1ae91a728e0cc63c8e6d3791ad
+}
+arm64(){
+  export SUPERCRONIC=supercronic-linux-arm64
+  export SUPERCRONIC_SHA1SUM=e2714c43e7781bf1579c85aa61259245f56dbba1
+}
+
+_install()
+{
+  cd /tmp
+  curl -fsSLO https://github.com/aptible/supercronic/releases/download/${VERSION}/${SUPERCRONIC}
+  echo "${SUPERCRONIC_SHA1SUM}  ${SUPERCRONIC}" | sha1sum -c -
+  chmod +x "$SUPERCRONIC"
+  mv "$SUPERCRONIC" /usr/local/bin/supercronic
+}
+
+case "${TARGETPLATFORM:-amd64}" in
+  arm64)
+    arm64
+    ;;
+  *arm$)
+    arm
+    ;;
+  amd64)
+    amd64
+    ;;
+  *)
+    echo TARGETPLATFORM not set, bailing log
+    exit 1
+    ;;
+esac
+
+_install
