808267f4e0d09bca393a57b13842e1697619c835
Tim Fletcher <tim@night-shade.org.uk>
2021-08-06 09:19:58 +0200

    Update base image and latest minor SAMBA release¡

--- samba-timemachine/spec/samba-timemachine/samba-timemachine_spec.rb
+++ samba-timemachine/spec/samba-timemachine/samba-timemachine_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 # Define packages
 packages = {
   samba: {
-    version: '2:4.13.4+dfsg-1'
+    version: '2:4.13.5+dfsg-2'
   },
   'samba-vfs-modules': {
-    version: '2:4.13.4+dfsg-1'
+    version: '2:4.13.5+dfsg-2'
   }
 }
 
